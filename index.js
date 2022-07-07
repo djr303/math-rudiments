e36c4a2a8a92a4e7c727bb01d88fa4022b9d0220
djr303 <david.j.roberts303@gmail.com>
2022-07-07 14:23:45 +0100

    WIP

---
+++ index.js
@@ -0,0 +1,96 @@
+const colors = require("colors");
+
+const ADDITION = "ADDITION";
+const SUBTRACTION = "SUBTRACTION";
+const MULTIPLICATION = "MULTIPLICATION";
+const DIVISION = "DIVISION";
+
+const settings = [
+  [ADDITION, 25, 100, 0],
+  [SUBTRACTION, 25, 100, 0],
+  [MULTIPLICATION, 25, 10, 0],
+  [DIVISION, 25, [100, 10], 0],
+];
+
+const generateFixed = (factor, toFixed) => parseInt(Math.round(Math.random() * factor).toFixed(toFixed))
+
+const generateAdditionProblem = (factor, toFixed) => {
+  const x = generateFixed(factor, toFixed);
+  const y = generateFixed(factor, toFixed);
+  const answer = x + y;
+  return [`${x} + ${y}`, `${answer}`];
+};
+const generateSubtractionProblem = (factor, toFixed) => {
+  const x = generateFixed(factor, toFixed);
+  const y = generateFixed(factor, toFixed);
+  const answer = x - y;
+  return [`${x} - ${y}`, `${answer}`];
+};
+const generateMultiplicationProblem = (factor, toFixed) => {
+  const x = generateFixed(factor, toFixed);
+  const y = generateFixed(factor, toFixed);
+  const answer = x * y;
+  return [`${x} * ${y}`, `${answer}`];
+};
+const generateDivisionProblem = (factor, toFixed) => {
+  const x = generateFixed(factor[0], toFixed);
+  let y = generateFixed(factor[1], toFixed)
+  y = y !== 0 ? y : 1;
+  const z = x * y
+  const answer = x
+  return [`${z} / ${y}`, `${answer}`];
+};
+
+const createQuestions = (s) => {
+  const ret = [];
+
+  for (let i = 0; i < s.length; i++) {
+    for (let x = 1; x <= s[i][1]; x++) {
+      switch (s[i][0]) {
+        case ADDITION:
+          ret.push(generateAdditionProblem(s[i][2], s[i][3]));
+          break;
+        case SUBTRACTION:
+          ret.push(generateSubtractionProblem(s[i][2], s[i][3]));
+          break;
+        case MULTIPLICATION:
+          ret.push(generateMultiplicationProblem(s[i][2], s[i][3]));
+          break;
+        case DIVISION:
+          ret.push(generateDivisionProblem(s[i][2], s[i][3]));
+          break;
+        default:
+          continue
+      }
+    }
+  }
+
+  return ret;
+};
+
+const keyPressContinue = async () => {
+  process.stdin.setRawMode(true);
+  return new Promise((resolve) =>
+    process.stdin.once("data", () => {
+      process.stdin.setRawMode(false);
+      resolve();
+    })
+  );
+};
+
+const main = async () => {
+  const questions = createQuestions(settings);
+
+  for (let i = 0; i < questions.length; i++) {
+    console.log(`${questions[i][0]} = ?\n`.red);
+    await keyPressContinue();
+    console.log(`${questions[i][1]}\n\n`.yellow);
+    await keyPressContinue();
+  }
+
+  console.log("Finished!".green);
+};
+
+;(async () => {
+  await main();
+})().then(process.exit)
